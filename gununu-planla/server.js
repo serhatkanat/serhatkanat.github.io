4c49eba7e7da4597fa2f941a8c7ea1ce529b2310
serhatkanat <173serhat@gmail.com>
2022-04-28 23:35:01 +0300

    popup delete

--- gununu-planla/server.js
+++
@@ -1,9 +0,0 @@
-APP.get('/manifest.json', function(req, res) {
-    res.append('Content-Type', 'text/javascript');
-    res.sendFile(path.join(root, "manifest.json"));
-});
-
-APP.get('/sw.js', function(req, res) {
-    res.append('Content-Type', 'text/javascript');
-    res.sendFile(path.join(root, "sw.js"));
-});
